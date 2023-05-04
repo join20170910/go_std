006cc7f2607a01201f95d86b2c7a28ff860982ac
john <17717990@qq.com>
2023-05-04 17:50:05 +0800

    Go 处理粘包拆包

---
+++ tcp-unpack/pack_new/server_new.go
@@ -0,0 +1,90 @@
+package tcp
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+)
+
+type buffer struct {
+	reader io.Reader
+	buf    []byte
+	start  int
+	end    int
+}
+
+func newBuffer(reader io.Reader, len int) buffer {
+	buf := make([]byte, len)
+	return buffer{reader, buf, 0, 0}
+}
+func (b *buffer) Len() int {
+	return b.end - b.start
+}
+
+//将有用的字节前移
+func (b *buffer) grow() {
+	if b.start == 0 {
+		return
+	}
+	copy(b.buf, b.buf[b.start:b.end])
+	b.end -= b.start
+	b.start = 0
+}
+
+//从reader里面读取数据，如果reader阻塞，会发生阻塞
+func (b *buffer) readFromReader() (int, error) {
+	b.grow()
+	n, err := b.reader.Read(b.buf[b.end:])
+	if err != nil {
+		return n, err
+	}
+	b.end += n
+	return n, nil
+}
+
+//返回n个字节，而不产生移位
+func (b *buffer) seek(n int) ([]byte, error) {
+	if b.end-b.start >= n {
+		buf := b.buf[b.start : b.start+n]
+		return buf, nil
+	}
+	return nil, errors.New("not enough")
+}
+
+//舍弃offset个字段，读取n个字段
+func (b *buffer) read(offset, n int) []byte {
+	b.start += offset
+	buf := b.buf[b.start : b.start+n]
+	b.start += n
+	return buf
+}
+func doConn_new(conn net.Conn) {
+	var (
+		buffer      = newBuffer(conn, 16)
+		headBuf     []byte
+		contentSize int
+		contentBuf  []byte
+	)
+	for {
+		_, err := buffer.readFromReader()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		for {
+			headBuf, err = buffer.seek(HEAD_SIZE)
+			if err != nil {
+				break
+			}
+			contentSize = int(binary.BigEndian.Uint16(headBuf))
+			if buffer.Len() >= contentSize-HEAD_SIZE {
+				contentBuf = buffer.read(HEAD_SIZE, contentSize)
+				fmt.Println(string(contentBuf))
+				continue
+			}
+			break
+		}
+	}
+}
