87417b1c7a4b1b375184e8e654d6e87394ef41c2
john <17717990@qq.com>
2023-05-12 22:43:38 +0800

    channel做为返回值

--- basic/channel/ch02/main.go
+++ basic/channel/ch02/main.go
@@ -10,6 +10,17 @@ func worker(id int, c chan int) {
 		fmt.Printf("Worker %d received %c\n", id, <-c)
 	}
 }
+
+// channel 作为返回值
+func createWorker(id int) chan int {
+	c := make(chan int)
+	go func() {
+		for {
+			fmt.Printf("Worker %d received %c\n", id, <-c)
+		}
+	}()
+	return c
+}
 func chanDemo() {
 	var channels [10]chan int
 	for i := 0; i < 10; i++ {
