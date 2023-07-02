75327952b96bb7aeea7e8956be5d23cc776e89ef
apple <17717990@qq.com>
2023-07-02 16:44:41 +0800

    学习案例- chan 管道的操作，创建 写入值 读取数据

--- basic/channel/demo02/main.go
+++ basic/channel/demo02/main.go
@@ -12,6 +12,9 @@ func main() {
 	intChan <- 10
 	num := 20
 	intChan <- num
+	intChan <- 50
+	// 注意不能存放大于容量的数据
+	intChan <- 500
 	fmt.Printf("实际长度:%v,容量:%v\n", len(intChan), cap(intChan))
 	fmt.Printf("%v,%v\n", <-intChan, <-intChan)
 }
