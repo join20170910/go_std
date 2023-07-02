acdbfc1bf45e8bc91312b4caadb364a636c8a61d
apple <17717990@qq.com>
2023-07-02 17:03:17 +0800

    学习案例- chan 管道的操作，关闭管道 可以读取数据

--- basic/channel/demo02/main.go
+++ basic/channel/demo02/main.go
@@ -3,6 +3,26 @@ package main
 import "fmt"
 
 func main() {
+	// demo01()
+	closed()
+}
+
+// 关闭功能读取数据测试
+func closed() {
+	var intChan chan int
+	// 初始化
+	intChan = make(chan int, 3)
+	fmt.Printf("intChan的值: %v\n", intChan)
+	fmt.Println("---------------------")
+	intChan <- 10
+	num := 20
+	intChan <- num
+	intChan <- 50
+	close(intChan)
+	fmt.Printf("%v,%v\n", <-intChan, <-intChan)
+}
+func demo01() {
+
 	// 定义 管道 声明管道 定义一个int 类型的管道
 	var intChan chan int
 	// 初始化
@@ -19,4 +39,5 @@ func main() {
 	fmt.Printf("%v,%v\n", <-intChan, <-intChan)
 	// 取数 操作 chan 长度 会报错
 	fmt.Printf("%v,%v\n", <-intChan, <-intChan)
+
 }
